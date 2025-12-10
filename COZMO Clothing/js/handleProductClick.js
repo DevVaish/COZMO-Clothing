8650d404ca009cae7277837413c03b9f353823ee
Vaishnavi <vaishnavi2073@gmail.com>
2025-12-10 21:27:07 +0530

    Delete COZMO Clothing directory

--- COZMO Clothing/js/handleProductClick.js
+++
@@ -1,8 +0,0 @@
-export default function handleProductClick() {
-  document.querySelectorAll(".pro").forEach((div) => {
-    div.addEventListener("click", () => {
-      window.document.location =
-        "sproduct.html" + "?productId=" + div.dataset.productId;
-    });
-  });
-}
